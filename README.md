43eff987d296cf8ffdda5ca7aacaf7d83ba2d9e5
ds5678 <jrpritts7@gmail.com>
2021-03-07 11:58:28 -0500

    Add project files.

---
+++ README.md
@@ -0,0 +1,10 @@
+﻿﻿# Crafting Hotkey Mod
+
+This mod for *The Long Dark* adds a hotkey for the crafting menu. Requires ModSettings.
+
+## Installation
+
+1. If you haven't done so already, install MelonLoader by downloading and running [MelonLoader.Installer.exe](https://github.com/HerpDerpinstine/MelonLoader/releases/latest/download/MelonLoader.Installer.exe)
+2. Download the latest version of `CraftingHotkey.dll` from the [releases page](https://github.com/ds5678/CraftingHotkey/releases)
+3. Download the latest version of `ModSettings.dll` from its [release page](https://github.com/zeobviouslyfakeacc/ModSettings/releases)
+4. Move `CraftingHotkey.dll` and `ModSettings.dll` into the Mods folder in your TLD install directory
---
+++ src/Settings.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModSettings;
+using UnityEngine;
+
+namespace CraftingHotkey
+{
+    internal enum KeyCodeAlphabet
+    {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G,
+        H,
+        I,
+        J,
+        K,
+        L,
+        M,
+        N,
+        O,
+        P,
+        Q,
+        R,
+        S,
+        T,
+        U,
+        V,
+        W,
+        X,
+        Y,
+        Z
+    }
+    internal class Settings : JsonModSettings
+    {
+        [Name("Key for the Crafting Menu")]
+        [Description("The key you press to show the crafting menu.")]
+        public KeyCodeAlphabet keyCodeAlphabet = KeyCodeAlphabet.T;
+
+        protected override void OnConfirm()
+        {
+            base.OnConfirm();
+            CraftingHotkeySettings.keyCode = (KeyCode)Enum.Parse(typeof(KeyCode), keyCodeAlphabet.ToString());
+        }
+    }
+    internal static class CraftingHotkeySettings
+    {
+        internal static readonly Settings settings = new Settings();
+        internal static KeyCode keyCode;
+
+        public static void OnLoad()
+        {
+            settings.AddToModSettings("Crafting Hotkey");
+            keyCode = (KeyCode)Enum.Parse(typeof(KeyCode), settings.keyCodeAlphabet.ToString());
+        }
+    }
+}
