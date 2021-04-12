c2b28ebf017200f3fcd80c0afbb78bcc5927c186
ds5678 <jrpritts7@gmail.com>
2021-04-12 18:10:11 -0400

    updated to use ModSettings 1.8

--- src/Implementation.cs
+++ src/Implementation.cs
@@ -1,76 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Harmony;
 using MelonLoader;
 using UnityEngine;
-using Harmony;
 
 namespace CraftingHotkey
 {
-    public class Implementation : MelonMod
-    {
-        public override void OnApplicationStart()
-        {
-            Debug.Log($"[{Info.Name}] Version {Info.Version} loaded!");
-            CraftingHotkeySettings.OnLoad();
-        }
+	public class Implementation : MelonMod
+	{
+		public override void OnApplicationStart()
+		{
+			Debug.Log($"[{Info.Name}] Version {Info.Version} loaded!");
+			Settings.OnLoad();
+		}
 
-        public static void MaybeShowCraftingMenu()
-        {
-            if (InputManager.GetKeyDown(InputManager.m_CurrentContext, CraftingHotkeySettings.keyCode))
-            {
-                Panel_Crafting craftingPanel = InterfaceManager.m_Panel_Crafting;
-                if (craftingPanel == null || uConsole.IsOn()) return;
-                if (craftingPanel.IsEnabled() && !craftingPanel.m_CraftingInProgress)
-                {
-                    craftingPanel.OnBackButton();
-                    return;
-                }
-                else
-                {
-                    Panel_FirstAid firstAid = InterfaceManager.m_Panel_FirstAid;
-                    if(firstAid != null && firstAid.IsEnabled())
-                    {
-                        firstAid.OnCraftingNav();
-                        return;
-                    }
-                    Panel_Clothing clothing = InterfaceManager.m_Panel_Clothing;
-                    if(clothing != null && clothing.IsEnabled())
-                    {
-                        clothing.OnCraftingNav();
-                        return;
-                    }
-                    Panel_Inventory inventory = InterfaceManager.m_Panel_Inventory;
-                    if(inventory != null && inventory.IsEnabled())
-                    {
-                        inventory.OnCraftingNav();
-                        return;
-                    }
-                    Panel_Log journal = InterfaceManager.m_Panel_Log;
-                    if(journal != null && journal.IsEnabled() && journal.m_ReadyForInput)
-                    {
-                        journal.OnCraftingNav();
-                        return;
-                    }
-                    Panel_Map map = InterfaceManager.m_Panel_Map;
-                    if(map != null &&  (map.IsEnabled() || !InterfaceManager.IsOverlayActiveImmediate()) )
-                    {
-                        map.OnCraftingNav();
-                        return;
-                    }
-                }
-            }
-        }
-    }
+		public static void MaybeShowCraftingMenu()
+		{
+			if (InputManager.GetKeyDown(InputManager.m_CurrentContext, Settings.options.keyCode))
+			{
+				Panel_Crafting craftingPanel = InterfaceManager.m_Panel_Crafting;
+				if (craftingPanel == null || uConsole.IsOn()) return;
+				if (craftingPanel.IsEnabled() && !craftingPanel.m_CraftingInProgress)
+				{
+					craftingPanel.OnBackButton();
+					return;
+				}
+				else
+				{
+					Panel_FirstAid firstAid = InterfaceManager.m_Panel_FirstAid;
+					if (firstAid != null && firstAid.IsEnabled())
+					{
+						firstAid.OnCraftingNav();
+						return;
+					}
+					Panel_Clothing clothing = InterfaceManager.m_Panel_Clothing;
+					if (clothing != null && clothing.IsEnabled())
+					{
+						clothing.OnCraftingNav();
+						return;
+					}
+					Panel_Inventory inventory = InterfaceManager.m_Panel_Inventory;
+					if (inventory != null && inventory.IsEnabled())
+					{
+						inventory.OnCraftingNav();
+						return;
+					}
+					Panel_Log journal = InterfaceManager.m_Panel_Log;
+					if (journal != null && journal.IsEnabled() && journal.m_ReadyForInput)
+					{
+						journal.OnCraftingNav();
+						return;
+					}
+					Panel_Map map = InterfaceManager.m_Panel_Map;
+					if (map != null && (map.IsEnabled() || !InterfaceManager.IsOverlayActiveImmediate()))
+					{
+						map.OnCraftingNav();
+						return;
+					}
+				}
+			}
+		}
+	}
 
-    [HarmonyPatch(typeof(GameManager),"Update")]
-    internal class GameManager_Update
-    {
-        private static void Postfix()
-        {
-            Implementation.MaybeShowCraftingMenu();
-        }
-    }
+	[HarmonyPatch(typeof(GameManager), "Update")]
+	internal class GameManager_Update
+	{
+		private static void Postfix()
+		{
+			Implementation.MaybeShowCraftingMenu();
+		}
+	}
 }
--- src/Settings.cs
+++ src/Settings.cs
@@ -1,63 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ModSettings;
+﻿using ModSettings;
 using UnityEngine;
 
 namespace CraftingHotkey
 {
-    internal enum KeyCodeAlphabet
-    {
-        A,
-        B,
-        C,
-        D,
-        E,
-        F,
-        G,
-        H,
-        I,
-        J,
-        K,
-        L,
-        M,
-        N,
-        O,
-        P,
-        Q,
-        R,
-        S,
-        T,
-        U,
-        V,
-        W,
-        X,
-        Y,
-        Z
-    }
-    internal class Settings : JsonModSettings
-    {
-        [Name("Key for the Crafting Menu")]
-        [Description("The key you press to show the crafting menu.")]
-        public KeyCodeAlphabet keyCodeAlphabet = KeyCodeAlphabet.T;
+	internal class CraftingHotkeySettings : JsonModSettings
+	{
+		[Name("Key for the Crafting Menu")]
+		[Description("The key you press to show the crafting menu.")]
+		public KeyCode keyCode = KeyCode.T;
+	}
+	internal static class Settings
+	{
+		internal static CraftingHotkeySettings options;
 
-        protected override void OnConfirm()
-        {
-            base.OnConfirm();
-            CraftingHotkeySettings.keyCode = (KeyCode)Enum.Parse(typeof(KeyCode), keyCodeAlphabet.ToString());
-        }
-    }
-    internal static class CraftingHotkeySettings
-    {
-        internal static readonly Settings settings = new Settings();
-        internal static KeyCode keyCode;
-
-        public static void OnLoad()
-        {
-            settings.AddToModSettings("Crafting Hotkey");
-            keyCode = (KeyCode)Enum.Parse(typeof(KeyCode), settings.keyCodeAlphabet.ToString());
-        }
-    }
+		public static void OnLoad()
+		{
+			options = new CraftingHotkeySettings();
+			options.AddToModSettings("Crafting Hotkey");
+		}
+	}
 }
